3e61b794d53534bb42a03be344f347cdc525bd3e
Kazuki Tsukamoto <kazuki050523@gmail.com>
2025-11-27 16:05:07 +0900

    first

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+
+	"log"
+	"os"
+	"uttc-hackathon-backend/dao"
+)
+
+func init() {
+	mysqlUser := os.Getenv("MYSQL_USER")
+	mysqlUserPwd := os.Getenv("MYSQL_USER_PWD")
+	mysqlDatabase := os.Getenv("MYSQL_DATABASE")
+
+	_db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@(localhost:3306)/%s", mysqlUser, mysqlUserPwd, mysqlDatabase))
+	if err != nil {
+		log.Fatalf("fail:sql.Open,%v/n", err)
+	}
+
+	defer _db.Close()
+
+	if err := _db.Ping(); err != nil {
+		log.Fatalf("fail:_db.Ping,%v/n", err)
+	}
+	log.Println("Connected to mysql")
+
+	userDAO := dao.NewUserDAO(_db)
+
+}
